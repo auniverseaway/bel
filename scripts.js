670e94f26e89c891ebb0320195f30421ae4d9c03
Chris Millar <cmillar@adobe.com>
2021-07-16 15:53:50 -0600

    PostLCP functions

--- scripts.js
+++ scripts.js
@@ -134,6 +134,37 @@ const blockLoader = (config, suppliedEl) => {
     init(parentEl);
 };
 
+const postLCP = () => {
+    const gtm = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K4JC9TV');`;
+
+    const element = document.createElement('script');
+    element.innerHTML = gtm;
+    document.querySelector('head').appendChild(element);
+};
+
+const setLCPTrigger = () => {
+    const lcpCandidate = document.querySelector('main > div:first-of-type > div:first-of-type img');
+    console.log(lcpCandidate);
+    if (lcpCandidate) {
+        if (lcpCandidate.complete) {
+            postLCP();
+        } else {
+            lcpCandidate.addEventListener('load', () => {
+                postLCP();
+            });
+            lcpCandidate.addEventListener('error', () => {
+                postLCP();
+            });
+        }
+    } else {
+        postLCP();
+    }
+};
+
 const config = {
     lazy: true,
     margin: '100px 0px',
@@ -154,5 +185,5 @@ const config = {
         }
     },
 };
-
+setLCPTrigger();
 blockLoader(config);
